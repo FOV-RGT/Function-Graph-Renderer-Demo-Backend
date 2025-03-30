225dfbdf93d82fd3c98c1c5f45bfd773b1392b1f
Almighty <kz@gdmu.edu.cn>
2025-03-31 00:06:29 +0800

    CROS

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const mathdataRouter = require('./routes/mathdata');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const avatarRouter = require('./routes/avatar');
-
+//CORS跨域配置
+const allowedOrigins = ['https://hanshu.kz2006.top']; // 允许的域名
 
 const adminUsersRouter = require('./routes/admin/users');
 const adminAuthRouter = require('./routes/admin/auth');
@@ -24,10 +25,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//CORS跨域配置
-app.use(cors({
-    origin: 'https://hanshu.kz2006.top' // 指定允许的来源
-}));
+
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin); 
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // 允许 Cookie
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DEL '); // 允许的方法
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // 允许的 Headers
+  next();
+});
 //路由配置
 app.use('/',indexRouter);
 app.use('/users', userAuth, usersRouter);
